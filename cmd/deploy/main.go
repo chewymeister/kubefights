d17e742d775ad9cc2a27340016c1e5dabe342d3b
Jonathan Qiwen Tsang <jtsang@pivotal.io>
2019-06-09 13:45:36 +0100

    Initial commit!
    Create some go binaries for deploy, and a directory for test
    
    At the moment, we can simply have the tool deploy some manifest files
    which then get tested for correctness. Once this has been achieved, we
    can think of a way to present the expectations in a question format.

---
+++ cmd/deploy/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := "/Users/JonT/.kube/config"
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, pod := range pods.Items {
+		fmt.Println("This is a Pod: ", pod)
+	}
+
+	fmt.Println(config)
+}
